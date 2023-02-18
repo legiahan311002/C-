d2669628223de50ea6aa449923ddec389faa64b3
legiahan311002 <101517508+legiahan311002@users.noreply.github.com>
2023-02-18 19:08:38 +0700

    Bài1_OOP

---
+++ BT OOP/BT OOP/Phanso.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BT_OOP
+{
+    public class Phanso
+    {
+        private int tuso;
+        private int mauso;
+
+        public Phanso()
+        {
+            tuso = 0;
+            mauso = 1;
+        }
+
+        public Phanso(int ts, int ms)
+        {
+            tuso = ts;
+            mauso = ms;
+        }
+
+        public void Nhap()
+        {
+            Console.Write("Nhap tu so: ");
+            tuso = int.Parse(Console.ReadLine());
+
+            do
+            {
+                Console.Write("Nhap mau so: ");
+                mauso = int.Parse(Console.ReadLine());
+            } while (mauso == 0);
+        }
+
+        public void Xuat()
+        {
+            Console.WriteLine("{0}/{1}", tuso, mauso);
+        }
+
+        public Phanso Cong(Phanso ps)
+        {
+            int ts = tuso * ps.mauso + mauso * ps.tuso;
+            int ms = mauso * ps.mauso;
+            Phanso kq = new Phanso(ts, ms);
+            return kq;
+        }
+
+        public Phanso Tru(Phanso ps)
+        {
+            int ts = tuso * ps.mauso - mauso * ps.tuso;
+            int ms = mauso * ps.mauso;
+            Phanso kq = new Phanso(ts, ms);
+            return kq;
+        }
+
+        public Phanso Nhan(Phanso ps)
+        {
+            int ts = tuso * ps.tuso;
+            int ms = mauso * ps.mauso;
+            Phanso kq = new Phanso(ts, ms);
+            return kq;
+        }
+
+        public Phanso Chia(Phanso ps)
+        {
+            int ts = tuso * ps.mauso;
+            int ms = mauso * ps.tuso;
+            Phanso kq = new Phanso(ts, ms);
+            return kq;
+        }
+    }
+}
---
+++ BT OOP/BT OOP/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BT_OOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Phanso ps1 = new Phanso();
+            Phanso ps2 = new Phanso();
+            ps1.Nhap();
+            ps2.Nhap();
+
+            Phanso tong = ps1.Cong(ps2);
+            Phanso hieu = ps1.Tru(ps2);
+            Phanso tich = ps1.Nhan(ps2);
+            Phanso thuong = ps1.Chia(ps2);
+
+            Console.WriteLine("Tong 2 phan so: ");
+            tong.Xuat();
+
+            Console.WriteLine("Hieu 2 phan so: ");
+            hieu.Xuat();
+
+            Console.WriteLine("Tich 2 phan so: ");
+            tich.Xuat();
+
+            Console.WriteLine("Thuong 2 phan so: ");
+            thuong.Xuat();
+        }
+    }
+}
