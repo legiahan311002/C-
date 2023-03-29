e301d62cee0141aa8463817b9797e682422c48ab
legiahan311002 <101517508+legiahan311002@users.noreply.github.com>
2023-03-29 22:35:21 +0700

    Tạo EF cơ bản (QLNhanVien)

---
+++ QLNhanVien/QLNhanVien/Controllers/NhanVienController.cs
@@ -0,0 +1,141 @@
+﻿using QLNhanVien.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QLNhanVien.Controllers
+{
+    public class NhanVienController : Controller
+    {
+        // GET: NhanVien
+        public ActionResult Index()
+        {
+            var listNhanVien = new DBNhanVienContext().NhanViens.ToList();
+            return View(listNhanVien);
+        }
+
+        // GET: NhanVien/Details/5
+        public ActionResult Permission(int id)
+        {
+            var context = new DBNhanVienContext();
+            var editing = context.NhanViens.Find(id);
+            return View(editing);
+        }
+        // POST: Permission/Edit/5
+        [HttpPost]
+        public ActionResult Permission(NhanVien model)
+        {
+            try
+            {
+                // TODO: Add update permissions logic here
+                var context = new DBNhanVienContext();
+                var oldItem = context.NhanViens.Find(model.Id);
+                oldItem.MatKhau = model.MatKhau;
+                oldItem.LaQuanTri = model.LaQuanTri;
+                oldItem.LaChuyenVien = model.LaChuyenVien;
+                oldItem.LaNhanVien = model.LaNhanVien;
+                context.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: NhanVien/Create
+        public ActionResult Create()
+        {
+            var context = new DBNhanVienContext();
+            var chucVuSelect = new SelectList(context.ChucVus, "Id", "TenChucVu");
+            ViewBag.IdChucVu = chucVuSelect;
+            return View();
+        }
+
+        // POST: NhanVien/Create
+        [HttpPost]
+        public ActionResult Create(NhanVien model)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                var context = new DBNhanVienContext();
+                context.NhanViens.Add(model);
+                context.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: NhanVien/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var context = new DBNhanVienContext();
+            var editing = context.NhanViens.Find(id);
+            var chucVuSelect = new SelectList(context.ChucVus, "Id", "TenChucVu",editing.IdChucVu);
+            ViewBag.IdChucVu = chucVuSelect;
+            return View(editing);
+        }
+
+        // POST: NhanVien/Edit/5
+        [HttpPost]
+        public ActionResult Edit(NhanVien model)
+        {
+            try
+            {
+                // TODO: Add update logic here
+                var context = new DBNhanVienContext();
+                var oldItem = context.NhanViens.Find(model.Id);
+                oldItem.HoVaTen = model.HoVaTen;
+                oldItem.GioiTinh = model.GioiTinh;
+                oldItem.Email = model.Email;
+                oldItem.IdChucVu = model.IdChucVu;
+                oldItem.SoCanCuoc = model.SoCanCuoc;
+                oldItem.SoDienThoai = model.SoDienThoai;
+                context.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: NhanVien/Delete/5
+        public ActionResult Delete(int id)
+        {
+            var context = new DBNhanVienContext();
+            var deleting  = context.NhanViens.Find(id);
+            return View(deleting);
+        }
+
+        // POST: NhanVien/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+
+                // TODO: Add delete logic here
+                var context = new DBNhanVienContext();
+                var deleting = context.NhanViens.Find(id);
+                context.NhanViens.Remove(deleting);
+                context.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ QLNhanVien/QLNhanVien/Models/NhanVien.cs
@@ -0,0 +1,53 @@
+﻿namespace QLNhanVien.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("NhanVien")]
+    public partial class NhanVien
+    {
+        public long Id { get; set; }
+
+        [Required]
+        [StringLength(200)]
+        [Display(Name = "Họ và tên")]
+        public string HoVaTen { get; set; }
+        [Display(Name = "Giới tính")]
+
+        public bool GioiTinh { get; set; }
+
+        [StringLength(200)]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [StringLength(20)]
+        [Display(Name = "Số điện thoại")]
+        public string SoDienThoai { get; set; }
+
+        [StringLength(200)]
+        [Display(Name = "Số căn cước")]
+        public string SoCanCuoc { get; set; }
+
+        [StringLength(200)]
+        [Display(Name = "Tên đăng nhập")]
+        public string TenDangNhap { get; set; }
+
+        [StringLength(200)]
+        [Display(Name = "Mật Khẩu")]
+        public string MatKhau { get; set; }
+        [Display(Name = "Chức vụ")]
+
+        public long? IdChucVu { get; set; }
+        [Display(Name = "Là quản trị viên")]
+        public bool LaQuanTri { get; set; }
+        [Display(Name = "Là chuyên viên")]
+        public bool LaChuyenVien { get; set; }
+        [Display(Name = "Là nhân viên")]
+        public bool LaNhanVien { get; set; }
+    
+        public virtual ChucVu ChucVu { get; set; }
+    }
+}
